aebb628c4dd4733f297d4a20a4949846ca7dccbf
Theo Breaux <theobreaux@gmail.com>
2023-02-23 12:43:49 -0800

    first commit

---
+++ like_button.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return React.createElement(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+
+document.querySelectorAll('.like_button_container')
+    .forEach(domContainer => {
+        const root = ReactDOM.createRoot(domContainer);
+        root.render(React.createElement(LikeButton));
+    });
+
+// const domContainer = document.querySelector('#like_button_container');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(React.createElement(LikeButton));
+
